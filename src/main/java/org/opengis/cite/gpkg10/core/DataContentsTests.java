f467bb02a51c29b6b5f33119cd16d6c258c15730
LLambertRGi <luke.lambert@rgi-corp.com>
2015-12-08 13:22:50 -0500

    requirement 9

--- src/main/java/org/opengis/cite/gpkg10/core/DataContentsTests.java
+++ src/main/java/org/opengis/cite/gpkg10/core/DataContentsTests.java
@@ -153,6 +153,33 @@ public class DataContentsTests extends CommonFixture
         fail(ErrorMessage.format(ErrorMessageKeys.NO_SQL_ACCESS));
     }
 
+    /**
+     * Every GeoPackage SQLite Configuration SHALL have the SQLite library
+     * compile and run time options specified in table <a href=
+     * "http://www.geopackage.org/spec/#every_gpkg_sqlite_config_table"> Every
+     * GeoPackage SQLite Configuration</a>.
+     *
+     * @see <a href="http://www.geopackage.org/spec/#_requirement-9" target=
+     *      "_blank">Every GPKG SQLite Configuration - Requirement 9</a>
+     *
+     * @throws SQLException
+     *             If an SQL query causes an error
+     */
+    @Test(description = "See OGC 12-128r12: Requirement 9")
+    public void sqliteOptions() throws SQLException
+    {
+        try(final Statement statement = this.databaseConnection.createStatement();
+            final ResultSet resultSet = statement.executeQuery("SELECT sqlite_compileoption_used('SQLITE_OMIT_*')"))
+        {
+            assertEquals(resultSet.getInt(1),
+                         0,
+                         ErrorMessage.format(ErrorMessageKeys.SQLITE_OMIT_OPTIONS));
+
+        }
+    }
+
+    
+
     private static final Pattern TEXT_TYPE = Pattern.compile("TEXT\\([0-9]+\\)");
     private static final Pattern BLOB_TYPE = Pattern.compile("BLOB\\([0-9]+\\)");
 
